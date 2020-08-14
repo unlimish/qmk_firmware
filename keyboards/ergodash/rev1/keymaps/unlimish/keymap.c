e8719e517e2069973decb8aa43a3f7c833b9c6ef
unlimish <14168376+unlimish@users.noreply.github.com>
2020-08-14 16:39:21 +0900

    Add: IME ON/OFF with Raise & Lower

--- keyboards/ergodash/rev1/keymaps/unlimish/keymap.c
+++ keyboards/ergodash/rev1/keymaps/unlimish/keymap.c
@@ -116,6 +116,9 @@ void persistent_default_layer_set(uint16_t default_layer) {
   default_layer_set(default_layer);
 }
 
+static bool lower_pressed = false;
+static bool raise_pressed = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
@@ -127,21 +130,37 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case LOWER:
       if (record->event.pressed) {
+        lower_pressed = true;
+
         layer_on(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
+        if (lower_pressed) {
+          register_code(KC_IMEOFF);
+          unregister_code(KC_IMEOFF);
+        }
+        lower_pressed = false;
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
+        raise_pressed = true;
+
         layer_on(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
+        if (raise_pressed) {
+          register_code(KC_IMEON);
+          unregister_code(KC_IMEON);
+        }
+        raise_pressed = false;
       }
       return false;
       break;
